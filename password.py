8825844845280dba879686cbfa8316ba55c96aae
Ruslan Sadykov <66594699+Interligo@users.noreply.github.com>
2020-10-30 10:31:00 +0300

    Task done

--- password.py
+++ password.py
@@ -6,5 +6,28 @@
 включая цифры и алфавитные символы
 """
 
+
+def password_evaluation():
+    user_password = input('Введите пароль: ')
+ 
+    password_requirements = {'digits': 0, 'capital letters': 0}
+ 
+    if len(user_password) >= 8 and user_password.isalnum():
+        for symbol in user_password:
+            if symbol.isdigit():
+                password_requirements['digits'] += 1
+            elif symbol.isupper():
+                password_requirements['capital letters'] += 1
+ 
+        if password_requirements['digits'] < 1:
+            print('Пароль простой. Рекомендуем добавить в пароль цифры.')        
+        elif password_requirements['capital letters'] < 1:
+            print('Пароль простой. Рекомендуем добавить в пароль буквы в верхнем регистре.')      
+        else:
+            print('Пароль сложный и отвечает всем требованиям безопасности.')
+    else:
+        print('Ваш пароль слишком простой. Он должен состоять, как минимум, из 8-ми алфавитных символов.')
+
+
 if __name__ == '__main__':
-    pass
+    password_evaluation()
