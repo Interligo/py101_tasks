989af82a9d497d3a7b838d21cf5a291bf3aab129
Ruslan Sadykov <66594699+Interligo@users.noreply.github.com>
2020-10-30 10:34:55 +0300

    Task done

--- guess_num.py
+++ guess_num.py
@@ -14,5 +14,42 @@
 Тебе может понадобится модуль random, цикл while и ветвления
 """
 
+
+def guess_digit(): 
+    number = random.randint(0, 1000000)
+    try_counter = 0
+
+    print('Привет!')
+    print('Число загадано. Попробуйте его отгадать.')    
+ 
+    while True:
+        answer = input('Введите число: ')
+    
+        if answer == '' or answer == 'exit':
+            print('Завершаю работу программы.')
+            break
+ 
+        if not answer.isdigit():
+            print('Пожалуйста, введите положительное число.')
+            continue
+    
+        if int(answer) > 1000000:
+            print('Пожалуйста, введите число в диапозоне от 0 до 1 000 000.')
+            continue
+ 
+        answer = int(answer)
+        try_counter += 1
+ 
+        if answer == number:
+            print(f'Верно! Вы отгадали число {number} за cледующее количество попыток - {try_counter}.')
+            break 
+        elif answer < number:
+            print('Загаданное число больше.')
+        else:
+            print('Загаданное число меньше.')
+
+
 if __name__ == '__main__':
-    pass
+    import random
+
+    guess_digit() 
