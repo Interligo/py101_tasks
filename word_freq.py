cfa4b7b8f699bbb9f72eddfe6f81d5e163949db5
Ruslan Sadykov <66594699+Interligo@users.noreply.github.com>
2020-10-30 21:48:26 +0300

    Work in progress

--- word_freq.py
+++ word_freq.py
@@ -8,5 +8,26 @@
 Тебе может понадобится модуль os, модуль argparse, цикл и словарь
 """
 
+def top100_word_counter(file_path):
+    # WORD_LIMIT = 100
+    
+    try:
+        with open(file_path, "r", encoding="UTF-8") as file:
+            stop_words = set(stopwords.words('english'))
+            words = []
+            TODO: добавить текст без стоп-слов в список words
+                
+    except IOError:
+        return 'Файл не найден.'
+    
+    TODO: добавить отфильтрованные слова в словарь и к ним значением добавить количество упоминаний в списке words
+        
+    TODO: отсортировать словарь по значениям по убыванию
+    ''' https://pythoner.name/sortdict - сортировка по значениям '''
+        
+    return 'Типа топ100 слов'
+
 if __name__ == '__main__':
-    pass
+    from nltk.corpus import stopwords
+    
+    print(top100_word_counter(file_path))    
