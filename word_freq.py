a57f958de895e97db17046e6d5a760721e4f22b7
Ruslan Sadykov <66594699+Interligo@users.noreply.github.com>
2020-11-03 13:03:25 +0300

    Task done

--- word_freq.py
+++ word_freq.py
@@ -8,26 +8,49 @@
 Тебе может понадобится модуль os, модуль argparse, цикл и словарь
 """
 
-def top100_word_counter(file_path):
-    # WORD_LIMIT = 100
-    
+
+def top100_word_counter(file_path, limit_words):
     try:
         with open(file_path, "r", encoding="UTF-8") as file:
-            stop_words = set(stopwords.words('english'))
-            words = []
-            TODO: добавить текст без стоп-слов в список words
-                
+            raw_data = file.read()
+            print('Файл найден. Загружаю...')
     except IOError:
-        return 'Файл не найден.'
-    
-    TODO: добавить отфильтрованные слова в словарь и к ним значением добавить количество упоминаний в списке words
-        
-    TODO: отсортировать словарь по значениям по убыванию
-    ''' https://pythoner.name/sortdict - сортировка по значениям '''
-        
-    return 'Типа топ100 слов'
+        return print('Файл не найден.')
+
+    # data cleaning
+    print('Анализирую текст...')
+    file_name, file_exp = file_path.split('.')
+    stop_words = set(stopwords.words('russian'))
+    tokens = word_tokenize(raw_data)
+    clean_data = [word.lower() for word in tokens if not word.lower() in stop_words and word.isalnum()]
+    words_counter = {}
+
+    # words counting
+    print('Считаю количество упоминаний...')
+    for word in clean_data:
+        if word in words_counter:
+            words_counter[word] += 1
+        else:
+            words_counter[word] = 1
+
+    # sorting data from dict
+    print('Сортирую слова по частоте употребления...')
+    print()
+    sorted_words_counter = list(words_counter.items())
+    sorted_words_counter.sort(key=lambda i: i[1], reverse=True)
+    sorted_words_counter = sorted_words_counter[:limit_words]
+
+    # printing limit_words words
+    print(f'В "{file_name}" наиболее часто повторялись следующие слова:')
+    print()
+    for (counter, item) in enumerate(sorted_words_counter, 1):
+        print(f'{counter}. "{item[0]}": {item[1]} р.')
+
 
 if __name__ == '__main__':
+    from nltk import word_tokenize
     from nltk.corpus import stopwords
+
+    top100_word_counter(file_path, limit_words)
+    # top100_word_counter('text_for_word_freq.txt', 100)
     
-    print(top100_word_counter(file_path))    
